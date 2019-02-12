dbad9942ccae5d3348e4f377e19619f1f1ca09c9
Alejandro Castro <alejandro.castro.q@gmail.com>
2019-02-12 10:11:09 -0300

    sandwich automatization

--- src/components/Sandwich/Sandwich.js
+++ src/components/Sandwich/Sandwich.js
@@ -1,16 +1,23 @@
 import React from "react";
 import SandwichIngredient from "./SandwichIngredients/SandwichIngredients";
 import Classes from "./Sandwich.css";
+
 const Sandwich = props => {
-  return (
-    <div className={Classes.Sandwich}>
-      <SandwichIngredient type={"bread-top"} />
-      <SandwichIngredient type={"salad"} />
-      <SandwichIngredient type={"cheese"} />
-      <SandwichIngredient type={"bacon"} />
-      <SandwichIngredient type={"meat"} />
-      <SandwichIngredient type={"bread-bottom"} />
-    </div>
-  );
+  const ingredients = Object.keys(props.ingredients)
+  .map(ingredient => {
+    return [...Array(props.ingredients[ingredient])]
+    .map( (_, index )=> {
+      return (<SandwichIngredient type={ingredient} key={ingredient + index}/>);
+    });
+  })
+  .reduce((array, element) => {
+    return array.concat(element)
+  } , []);
+
+  return (<div className={Classes.Sandwich}>
+    <SandwichIngredient type={"bread-top"}/> 
+    {ingredients.length ?ingredients:<p>empty</p>}
+    <SandwichIngredient type={"bread-bottom"}/>
+  </div>);
 };
 export default Sandwich;
--- src/containers/SandwichBuilder/SandwichBuilder.js
+++ src/containers/SandwichBuilder/SandwichBuilder.js
@@ -2,11 +2,20 @@ import React from "react";
 import Wrapper from "../../hoc/Wrapper";
 import Sandwich from "../../components/Sandwich/Sandwich";
 class SandwichBuilder extends React.Component {
+  state = {
+    ingredients:{
+      salad:0,
+      bacon:0,
+      cheese:0,
+      meat:0,
+    }
+  }
+
   render() {
     return (
       <Wrapper>
         <div>sandwich control panel</div>
-        <Sandwich />
+        <Sandwich ingredients={this.state.ingredients} />
       </Wrapper>
     );
   }
